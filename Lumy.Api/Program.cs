7b140ea8387d154013d66fc20ef7fde4dfda08cf
hgdev <hgdevr@gmail.com>
2026-01-10 16:10:23 -0300

    Correcciones codigo e instrucciones

--- Lumy.Api/Program.cs
+++ Lumy.Api/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ContextoLumy>(opc => 
-    opc.UseSqlServer(builder.Configuration.GetConnectionString("ConexionPorDefecto")));
+    opc.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddHttpClient<IServicioLibros, ServicioLibros>();
 builder.Services.AddControllers();
